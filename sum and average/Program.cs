18b5af9e336a37df085d572048a03455983a9929
Jony Sarowar <42883973+jony-sarowar@users.noreply.github.com>
2024-10-01 20:59:33 +0600

    Add files via upload

---
+++ sum and average/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Test{
+  public static void Main(string[] args){
+
+    int number1, number2, number3, sum;
+    double average;
+
+    Console.Write("number1 = ");
+    number1 = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("number2 = ");
+    number2 = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("number3 = ");
+    number3 = Convert.ToInt32(Console.ReadLine());
+
+    sum = number1 + number2 + number3;
+    Console.WriteLine($"sum = {sum}");
+
+    average = (double)sum / 3;
+    Console.WriteLine($"average = {average. ToString("F2")}");
+
+  }
+
+}
